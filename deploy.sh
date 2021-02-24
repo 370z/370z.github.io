83a9769560a2cff3a7577bbf7c8ea89d2cdce390
intelligent.farming.platform@gmail.com <intelligent.farming.platform@gmail.com>
2021-02-24 18:31:12 +0700

    55

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
 # abort on errors
-set -e
+# set -e
 
 # build
 npm run build
 
 # navigate into the build output directory
-# cd dist
+cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
