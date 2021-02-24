acfa8c105e19826c0c0c1f7e2ca12f94ea16ad06
intelligent.farming.platform@gmail.com <intelligent.farming.platform@gmail.com>
2021-02-24 17:59:01 +0700

    222

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@
 
 module.exports = {
     transpileDependencies: ["vuetify"],
-    publicPath: process.env.NODE_ENV === "production" ? "/public" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/370z.github.io/" : "/",
 };
